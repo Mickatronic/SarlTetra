6de73ecfa663c8985959d9b01a4614141b6d55c2
tchouani laetitia <laetitia.tchouani@gmail.com>
2015-03-13 09:02:45 +0100

    Ajouter code creer client
    Ajouter code creer client

--- www/CreerClient.php
+++ www/CreerClient.php
@@ -15,7 +15,82 @@
 					Menu
 				</div>
 				<div class="col-sm-10">
-					Section
+					<div class="row">
+										<div class="col-sm-2">
+											<form class="form-horizontal">
+												<fieldset>
+													
+													<div class="form-group">
+														<label class="control-label" for="">Nom:</label>
+															<div class="controls">
+																<input id="" name="" type="text" placeholder="" class="form-control input-small">
+															</div>
+													</div>
+													
+													<div class="form-group">
+														<label class="control-label" for="">Prenom:</label>
+															<div class="controls">
+																<input id="" name="" type="text" placeholder="" class="form-control input-small">
+															</div>
+													</div>
+														
+													<div class="form-group">
+														<label class="control-label" for="">civilite:</label>
+															<div class="controls">
+																<input id="" name="" type="text" placeholder="" class="form-control input-small">
+															</div>
+													</div>
+
+													<div class="form-group">
+														<label class="control-label" for="">Age:</label>
+															<div class="controls">
+																<input id="" name="" type="text" placeholder="" class="form-control input-small">
+															
+															</div>
+													</div>
+											
+													<div class="form-group">
+														<label class="control-label" for="">Ville:</label>
+															<div class="controls">
+																<input id="" name="" type="text" placeholder="" class="form-control input-small">
+																
+															</div>
+													</div>
+
+													<div class="form-group">
+														<label class="control-label" for="">CP:</label>
+															<div class="controls">
+																<input id="" name="" type="text" placeholder="" class="form-control input-small">
+																
+															</div>
+													</div>
+
+													<div class="form-group">
+														<label class="control-label" for="">Telephone:</label>
+															<div class="controls">
+																<input id="" name="" type="text" placeholder="" class="form-control input-small">
+																
+															</div>
+													</div>
+
+													<div class="form-group">
+														<label class="control-label" for="">Email:</label>
+															<div class="controls">
+																<input id="" name="" type="text" placeholder="" class="form-control input-small">
+																
+															</div>
+													</div>
+												</fieldset>
+												    <div class="form-group">
+														<label class="col-md-4 control-label" for="">Créer</label>
+															<div class="col-md-8">
+																<button id="" name="" class="btn btn-success">envoyer</button>
+																<button id="" name="" class="btn btn-danger">annuler</button>
+															</div>
+													</div>
+											</form>
+										</div>
+									</div>
 					
 					
 				</div>
