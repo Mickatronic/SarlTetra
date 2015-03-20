7a55c0ef761f7f89be55babfdb269e516491034b
Mickatronic <honvault.m@gmail.com>
2015-03-20 07:57:19 +0100

    Créer Civilités - Connexion MySQL
    Créer Civilités - Connexion MySQL

--- www/creercivilite.php
+++ www/creercivilite.php
@@ -10,9 +10,43 @@
 			include 'navbar.php';
 		?>
 		<div class="container">
-		<form class="form-horizontal">
+		<div class="form-horizontal">
 			<fieldset>
 				<div class="form-group">
+				<form method="GET" action="CreerCivilite-Validation.php" class="col-sm-12 jumbotron">
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="row">
+								<div class="form-horizontal">
+									
+									<div class="form-group">
+									  <label class="col-sm-2 control-label" for="textinput">Abreviation:</label>  
+									  <div class="col-sm-8 input-group">
+										<span class="input-group-addon"> <span class="glyphicon glyphicon-user"></span></span>
+										<input id="Abreviation" name="Abreviation" placeholder="ex : Mr" class="form-control input-md" type="text"/>
+									  </div>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="form-horizontal">
+									
+									<div class="form-group">
+									  <label class="col-sm-2 control-label" for="textinput">Libelle:</label>  
+									  <div class="col-sm-8 input-group">
+										<span class="input-group-addon"> <span class="glyphicon glyphicon-user"></span></span>
+										<input id="Libelle" name="Libelle" placeholder="ex : Monsieur" class="form-control input-md" type="text"/>
+									  </div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				    <div class="col-md-4">
+						<input type="submit" name="Envoyer" value="Créer Civilité" class="btn btn-primary" />
+					</div>
+				</div>
+				</form>
 				  <label class="col-md-4 control-label" for="">civilite</label>
 				  <div class="col-md-4"> 
 					<label class="radio-inline" for="-0">
@@ -36,6 +70,6 @@
 				  </div>
 				</div>
 			</fieldset>
-		</form>
+		</div>
 	</body>
 </html>		
