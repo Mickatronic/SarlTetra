cc068a697dc670889415631e7323e309137a98ef
Mickatronic <honvault.m@gmail.com>
2015-03-20 07:37:11 +0100

    Ajout du script de la base de données
    Ajout du script de la base de données

---
+++ www/CreerClient-Validation.php
@@ -0,0 +1,18 @@
+<?php
+	// Déclaration variable
+	$nom = "";
+	$prenom="";
+	$civilite="";
+	$Age="";
+	$Ville="";
+	$CP="";
+	$Tel="";
+	$Email="";
+	
+	// Récupération des données
+	
+	// Traitement
+	
+	// Sortie
+
+?>
