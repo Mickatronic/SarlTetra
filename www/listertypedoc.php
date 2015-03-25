274d372ad47c9986daa2e4ad49ddb5db7e314557
tchouani laetitia <laetitia.tchouani@gmail.com>
2015-03-25 22:21:51 +0100

    modif document

--- www/listertypedoc.php
+++ www/listertypedoc.php
@@ -15,25 +15,13 @@
 				<div class="form-group">
 				  <label class="col-md-4 control-label" for="">Lister Type Document</label>
 				  <div class="col-md-4">
-					<select id="" name="" class="form-control">
-					  <option value="1">Facture</option>
-					  <option value="2">Devis</option>
-					  <option value="">Schéma</option>
-					  <option value="">Autre</option>
+					<P> Facture<br/>
+					    Devis<br/>
+						Schéma</p>
 					</select>
 				  </div>
 				</div>
 				</fieldset>
-				<form class="form-horizontal">
-					<fieldset>
-					<div class="form-group">
-					  <label class="col-md-4 control-label" for=""></label>
-					  <div class="col-md-4">
-						<button id="" name="" class="btn btn-primary">Sélectionner</button>
-					  </div>
-					</div>
-					</fieldset>
-					</form>
 			</form>
      </body>
 </html>
